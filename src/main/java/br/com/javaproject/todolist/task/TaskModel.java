42e572a8b38bca6c41c507d6d0dce2d16093a101
Michel <michel.geronimo@hotmail.com>
2023-10-15 10:26:59 -0300

    first commit

---
+++ src/main/java/br/com/javaproject/todolist/task/TaskModel.java
@@ -0,0 +1,47 @@
+package br.com.javaproject.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    /**
+     * ID
+     * Usuario (ID_Usuario)
+     * Descrição
+     * Titulo
+     * Data inicio / termino
+     * Prioridade
+     */
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception{
+        if(title.length() > 50){
+            throw new Exception("O campo Title deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
+}
---
+++ src/test/java/br/com/javaproject/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javaproject.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
